37cb2dc6e993119a83bc512cf69124da29d4e0f6
Andrey Posnov <aposnov@wakeapp.ru>
2020-01-21 00:20:08 +0300

    Initial Commit

---
+++ dollarbit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dollarbit
+//
+//  Created by Andrey Posnov on 21.01.2020.
+//  Copyright © 2020 Andrey Posnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
